544e02c145dd930621d06b7cee013c94b9b1d7fc
agnaldo marinho <agnaldomarinho7@gmail.com>
2019-06-06 23:08:44 -0300

    Adicionando o script de execucao de container

--- exec-container.sh
+++ exec-container.sh
@@ -1,5 +1,12 @@
+# Gerando a image
 docker build -t desafio-devops -f Dockerfile . --no-cache
 
+# Rodando o container na porta 5000
 docker run -d -p 5000:5000  desafio-devops
 
+# Listando container em execucao
 docker ps
+
+# Teste via linha de comando usando curl
+ sleep 2
+ curl http://127.0.0.1:5000/
